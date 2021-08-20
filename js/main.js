5ec7f70ea93dd60ec43f8bf4eb79ab5ad16fc4db
187PM14029 - Trinh Minh Trung - K24T-PM1 <trung.187pm14029@vanlanguni.vn>
2021-08-20 14:25:09 +0700

    upload source code

---
+++ js/main.js
@@ -0,0 +1,36 @@
+// const swiper = new Swiper('.swiper-container', {
+
+//     navigation: {
+//         nextT1: '.swiper-button-next',
+//         prevT1: '.swiper-button-previous',
+//     },
+
+//     autoplay:
+//     {
+//         delay: 2000,
+//     },
+//     loop: true,
+
+//     //Default parameters
+//     slidesPerView: 1,
+//     spaceBetween: 10,
+//     centeredSlides: true,
+//     roundLengths: true,
+//     loop: true,
+//     loopAdditionalSlides: 30,
+//     breakpoints: {
+//         320: {
+//             slidesPerView: 1,
+//             spaceBetween: 0
+//         },
+//         480: {
+//             slidesPerView: 2,
+//             spaceBetween: 0,
+//         },
+//         640: {
+//             // xem số lượng thể hiện
+//             slidesPerView: 3,
+//             spaceBetween: 0,
+//         },
+//     }
+// });
